2d2628bc597487ee2fd007230bfca3efcdce2997
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-03-07 16:07:51 +0100

    Extend hello example

--- crates/family/examples/hello.rs
+++ crates/family/examples/hello.rs
@@ -2,16 +2,32 @@ use family::any::{AnyMember, FamilyMember};
 use family_derive::Member;
 
 #[derive(Member)]
-struct Borrowed<'a>(&'a str);
+struct Borrowed<'a>(&'a mut String);
+
+#[derive(Member)]
+struct Owned(String);
 
 fn main() {
-    let value = "Family".to_string();
-    let value = Borrowed(value.as_str());
+    let mut string = "Family".to_string();
+
+    let value = Borrowed(&mut string);
     let boxed = Box::new(FamilyMember::<BorrowedF>(value));
-    say_hello(boxed);
+    say_hello_borrowed(boxed);
+
+    let value = Owned(string);
+    let boxed = Box::new(FamilyMember::<Owned>(value));
+    say_hello_owned(boxed);
 }
 
-fn say_hello<'a>(value: Box<dyn AnyMember<'a> + 'a>) {
+fn say_hello_borrowed<'a>(value: Box<dyn AnyMember<'a> + 'a>) {
     let value: Borrowed = value.downcast::<BorrowedF>().expect("downcast failed").0;
+    println!("Hello, {}!", value.0);
+
+    // Since it's a mutable borrow, we can change it too
+    *value.0 = "Changed".to_string();
+}
+
+fn say_hello_owned<'a>(value: Box<dyn AnyMember<'a> + 'a>) {
+    let value: Owned = value.downcast::<Owned>().expect("downcast failed").0;
     println!("Hello, {}!", value.0);
 }
